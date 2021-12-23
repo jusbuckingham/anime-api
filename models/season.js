aed9dd675fbde8ef3490cf6fd3018781d6e43ed3
jusbuckingham <jus.buckingham@gmail.com>
2021-12-23 14:10:37 -0800

    all changes

---
+++ models/season.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const seasonSchema = new mongoose.Schema({
+    url: String,
+    title: { type: String, required: true },
+    image_url: String,
+    synopsis: String,
+});
+
+const Season = mongoose.model("Season", seasonSchema);
+
+module.exports = Season; 
+
